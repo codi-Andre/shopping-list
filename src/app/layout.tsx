ca28b1731e71fa900dc3f3ae073fc8e12424725e
André Miranda <aegis.andre@gmail.com>
2023-05-10 23:24:49 -0300

    feat: static home page and change theme colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
-import './globals.css'
+import { Header } from '@/components/header';
+import './globals.css';
+import Image from 'next/image';
+import bg_book from '@/assets/backgrounds/book.png';
 
 export const metadata = {
   title: 'Shopping List Web App',
   description: 'Plan ahead results in time save and better prices',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body>
+        <div className='app'>
+          <Header />
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export function Header() {
+  return (
+    <header className='gap-4 flex flex-col'>
+      <h1 className='text-4xl text-center'>Shopping List WebApp</h1>
+
+      <p className='text-center'>
+        Plan ahead results in time save and better prices
+      </p>
+    </header>
+  );
+}
