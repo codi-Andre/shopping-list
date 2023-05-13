a06e39f73ae5b767b3d3759a601253724c469884
André Miranda <aegis.andre@gmail.com>
2023-05-12 23:32:34 -0300

    feat: add form and state

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,27 @@
-import './globals.css';
-import { Header } from '@/components/header';
-import { BgImages } from '@/components/BgImages';
+import './globals.css'
+
+import { Header } from '@/app/components/header'
+import { BgImages } from '@/app/components/BgImages'
 
 export const metadata = {
   title: 'Shopping List Web App',
   description: 'Plan ahead results in time save and better prices',
-};
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body>
         <BgImages />
-        <div className='app'>
+        <div className="app">
           <Header />
           {children}
         </div>
       </body>
     </html>
-  );
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,98 @@
+'use client'
+
+import { FormEvent, useRef, useState } from 'react'
+
+interface ListItemData {
+  [index: string]: string | number
+
+  name: string
+  quantity: number
+  price: number
+}
+
+interface InputData {
+  current: {
+    value: ''
+  }
+}
+
 export default function Home() {
+  const itemRef = useRef<HTMLInputElement>(null)
+  const quantityRef = useRef<HTMLInputElement>(null)
+  const priceRef = useRef<HTMLInputElement>(null)
+  const [list, setList] = useState<ListItemData[]>([])
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault()
+
+    const form = e.target as HTMLFormElement
+    const formData = new FormData(form)
+    const listItem = Object.fromEntries(formData.entries()) as ListItemData
+
+    setList(list => [...list, { ...listItem }])
+    itemRef.current!.value = ''
+    quantityRef.current!.value = ''
+    priceRef.current!.value = ''
+  }
+
   return (
-    <main className=''>
-      <h2 className='text-3xl'>Quick List</h2>
-      <ul className='list-inside list-disc'>
-        <li className='list-item'>beans</li>
-        <li className='list-item'>rice</li>
-        <li className='list-item'>meat</li>
+    <main className="flex flex-col gap-4 p-4">
+      <h2 className="text-3xl">Quick List</h2>
+
+      <ul className="list-inside list-disc">
+        {list.map((item, index) => {
+          return (
+            <li
+              key={`${item.name}-${index}`}
+              className="list-item border-b-2 border-solid border-yellow-7"
+            >
+              <span>{item.name}</span>
+              <span>{item.quantity}</span>
+              <span>{item.price}</span>
+            </li>
+          )
+        })}
       </ul>
+
+      <form onSubmit={handleSubmit} className="flex justify-center gap-4">
+        <input
+          className="rounded-md bg-yellow-5 p-2 placeholder:text-white"
+          ref={itemRef}
+          type="text"
+          placeholder="Item name"
+          name="name"
+        />
+
+        <input
+          className="rounded-md bg-yellow-5 p-2 placeholder:text-white"
+          ref={quantityRef}
+          type="number"
+          placeholder="Item quantity"
+          name="quantity"
+        />
+
+        <input
+          className="rounded-md bg-yellow-5 p-2 placeholder:text-white"
+          ref={priceRef}
+          type="number"
+          step="0.01" // allows two decimal places
+          placeholder="Item price"
+          name="price"
+        />
+
+        <button
+          className="rounded-md border-2 border-solid border-yellow-7 bg-yellow-5 p-2 hover:bg-yellow-7"
+          type="submit"
+        >
+          Add
+        </button>
+        <button
+          className="rounded-md border-2 border-solid border-yellow-7 bg-yellow-5 p-2 hover:bg-yellow-7"
+          type="reset"
+        >
+          Cancel
+        </button>
+      </form>
     </main>
-  );
+  )
 }
